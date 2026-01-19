ddc1a4448007d37c68d428d2aa517203e72966e0
Bart Simpson <bart_s2005@tut.by>
2026-01-19 13:14:45 +0300

    datetime update to 2026-01-19 13:14:45

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DATE=`date +"%Y-%m-%d %H:%M:%S"`
-cat src/index.html| sed "s/id='upd' datetime='2026.01.19 12:54:47'>.*</id='upd' datetime='${DATE}'>${DATE}</g"
+FILE="$DIR"/src/index.html
+sed "s/id='upd' datetime='2026.01.19 12:54:47'>.*</id='upd' datetime='${DATE}'>${DATE}</g" -i "$FILE"
 git commit -a -m "datetime update to ${DATE}"
-# git push
+git push
