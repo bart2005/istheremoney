fb21dcfe0f388c9c81af5ab2256ee47b1e7c115b
Bart Simpson <bart_s2005@tut.by>
2026-01-20 22:45:29 +0300

    autoupdate todo,plan,state

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,20 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DATE=`date +"%Y-%m-%d %H:%M:%S"`
 FILE="$DIR"/src/index.html
+
+function update_time() {
+  rgx="$1"
+  file_to_update=$(rg -l "$rgx" src)
+  echo "$file_to_update"
+  modify_date=$(stat -c "%Y" "$file_to_update" | xargs -I[] date -d @[] +"%Y-%m-%d %H:%M:%S")
+  echo $modify_date
+  sed "s/$rgx datetime=.*</$rgx datetime='${modify_date}'>${modify_date}</g" -i "$file_to_update"
+}
+
 echo "update time to $DATE"
 sed "s/id='upd' datetime=.*</id='upd' datetime='${DATE}'>${DATE}</g" -i "$FILE"
-git commit -a -m "datetime update to ${DATE}"
-git push
+
+update_time "id='update-todo'"
+# update_time "id='update-plan'"
+# git commit -a -m "datetime update to ${DATE}"
+# git push
