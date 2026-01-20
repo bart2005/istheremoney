36624b7ebf77f1d12527cfada05ef48657683d42
Bart Simpson <bart_s2005@tut.by>
2026-01-20 23:15:36 +0300

    modify update time to check real update

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
-set -e
+# set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FORMAT="%Y.%m.%d %H:%M:%S"
 DATE=`date +"$FORMAT"`
 FILE="$DIR"/src/index.html
-
+cd "$DIR"
 function update_time() {
-  rgx="$1"
-  file_to_update=$(rg -l "$rgx" src)
-  echo "$file_to_update"
-  modify_date=$(stat -c "%Y" "$file_to_update" | xargs -I[] date -d @[] +"$FORMAT")
-  echo $modify_date
-  sed "s/$rgx datetime=.*</$rgx datetime='${modify_date}'>${modify_date}</g" -i "$file_to_update"
+  FILES_MD5=".files_md5"
+  RGX="$1"
+  FILE_TO_UPDATE=$(rg -l "$RGX" src)
+  echo "update time for file: $FILE_TO_UPDATE"
+  MODIFY_DATE=$(stat -c "%Y" "$FILE_TO_UPDATE" | xargs -I[] date -d @[] +"$FORMAT")
+  echo file modify time: $MODIFY_DATE
+  LAST_FILE_HASH=$(rg "$FILE_TO_UPDATE" "$FILES_MD5")
+  if [[ -z "$LAST_FILE_HASH" ]]; then echo write md5sum;  md5sum "$FILE_TO_UPDATE" | tee -a "$FILES_MD5"; fi
+  CURR_FILE_HASH=$(md5sum "$FILE_TO_UPDATE")
+  echo LAST_FILE_HASH: $LAST_FILE_HASH
+  echo CURR_FILE_HASH: $CURR_FILE_HASH
+  if [[ "$LAST_FILE_HASH" == "$CURR_FILE_HASH" ]]
+  then
+    echo file "$FILE_TO_UPDATE was not updated"
+  else
+    sed "s/$RGX datetime=.*</$RGX datetime='${MODIFY_DATE}'>${MODIFY_DATE}</g" -i "$FILE_TO_UPDATE"
+    CURR_FILE_HASH=$(md5sum "$FILE_TO_UPDATE")
+    echo updated file hash:$CURR_FILE_HASH
+    sed "s|.*$FILE_TO_UPDATE|$CURR_FILE_HASH|g" -i "$FILES_MD5"
+  fi
 }
 
-echo "update time to $DATE"
-sed "s/id='upd' datetime=.*</id='upd' datetime='${DATE}'>${DATE}</g" -i "$FILE"
+echo "update site time to $DATE"
+# sed "s/id='upd' datetime=.*</id='upd' datetime='${DATE}'>${DATE}</g" -i "$FILE"
 
 update_time "id='update-todo'"
 update_time "id='update-plan'"
